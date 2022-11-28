98920eeadbb561856b8c22aededd2713c0e05be0
Antonio Ochavo <Antonio.Ochavo@solera.com>
2022-11-28 14:33:16 +0100

    Unit Test Project

---
+++ tests/learning-teamcity-api-tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace learning_teamcity_api_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
